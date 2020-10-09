f2becdb3c7eedffde93d664909a0adee8840ae2b
Daniel Šerý <daniel@turing-technology.com>
2020-10-09 17:14:25 +0200

    Removed not needed code

--- RiddleSolve/Constants.cs
+++ RiddleSolve/Constants.cs
@@ -5,8 +5,5 @@
         public const int TileSize = 298;
         public const int SmileDistance = 77;
         public const int ColorDistance = 100;
-        
-        public const double DoubleHalfTileSize = 149;
-        public const double DoubleTileSize = 298;
     }
 }
